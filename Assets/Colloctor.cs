bd09b37ff2562e3763045dfdc78d7af9c5274aff
Sidon Kodraliu Korcaj <sidonkk@gmail.com>
2016-04-09 16:52:54 +0200

    something

---
+++ Assets/Colloctor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Colloctor : MonoBehaviour {
+
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.CompareTag ("DynamicParticle")) {
+			Destroy (col.gameObject);
+		}
+			
+	
+	}
+}
