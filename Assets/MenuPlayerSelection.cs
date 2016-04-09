9fbb3b8b62ea446455c79ea08f618c90a6b9e56a
Peter Scheel <Peter Scheel>
2016-04-09 23:07:27 +0200

    Checkpoint

---
+++ Assets/MenuPlayerSelection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuPlayerSelection : MonoBehaviour {
+
+	public int PlayerController;
+	public bool joined = false;
+
+	public Text joinedText;
+	public Text notJoinedText;
+	public MenuAnimation ma;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("PS4_X" + PlayerController)) {
+			joined = true;
+			joinedText.enabled = true;
+			notJoinedText.enabled = false;
+			ma.active = true;
+		}
+		if (Input.GetButton ("PS4_O" + PlayerController)) {
+			joined = false;
+			joinedText.enabled = false;
+			notJoinedText.enabled = true;
+			ma.active = false;
+		}
+	}
+}
---
+++ Assets/MenuPlayerSelectionScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPlayerSelectionScreen : MonoBehaviour {
+
+	public MenuAnimation[] players;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 1; i < 5; i++) {
+			if (Input.GetButton ("PS4_Start" + i)) {
+				print ("Start");
+			}
+		}
+	}
+
+	private void StartGame(){
+		 
+	}
+}
