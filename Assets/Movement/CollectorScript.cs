aa25bb3c9c3d194c1328e106821d9ccf60b7722e
bhas <g.bvinther@gmail.com>
2016-04-09 23:49:03 +0200

    water stuff

---
+++ Assets/Movement/CollectorScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectorScript : MonoBehaviour {
+
+	public int maxWater = 10;
+	float size;
+	public int water = 0;
+
+	void Start() {
+		size = GetComponent<BoxCollider2D> ().bounds.extents.y * 2;
+	}
+
+	void OnCollisionEnter2D(Collider2D col) {
+		// if hitting water 
+		if (col.CompareTag("DynamicParticle") && water < maxWater) {
+			water++;
+			Destroy (col.gameObject);
+		}
+
+		// if being hit by a player from above
+		if (col.CompareTag("Player")) {
+			float y = col.gameObject.transform.position.y;
+			float boundY = transform.position.y + size - 0.001f;
+			if (y >= boundY) {
+				// some death animation
+				// insert code here
+				print("death");
+				water = 0;
+			}
+
+		}
+	}
+}
