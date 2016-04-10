8e4edd14656d35ba22c3da130c02630a02ef77ce
Peter Scheel <Peter Scheel>
2016-04-10 12:06:52 +0200

    no message

--- Assets/Tank2.cs
+++ Assets/Tank2.cs
@@ -6,10 +6,17 @@ public class Tank2 : MonoBehaviour {
 	public Transform spawn;
 	public int CountWater;
 
+	SoundSystem sound;
+
+	void Start(){
+		sound = GameObject.Find ("Controller").GetComponent<SoundSystem> ();
+	}
+
 	void OnTriggerEnter2D(Collider2D col){
 	
 		if (col.CompareTag ("Team2") && col.gameObject.GetComponent<CollectorScript>().water > 0) {
-			
+			sound.PlaySound ("Throw");
+
 			col.gameObject.GetComponent<CollectorScript>().water--;
 			CountWater++;
 			GameObject newLiquidParticle=(GameObject)Instantiate(Resources.Load("LiquidPhysics/DynamicParticle 1")); //Spawn a particle
