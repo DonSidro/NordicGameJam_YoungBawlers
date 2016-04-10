3ddcd1fe4d3d01c3ed724d0312d2f9ebacbf9aa9
Peter Scheel <Peter Scheel>
2016-04-10 11:44:18 +0200

    dfasdfasd

--- Assets/TankAmout.cs
+++ Assets/TankAmout.cs
@@ -7,13 +7,7 @@ public class TankAmout : MonoBehaviour {
 
 
 	void Update () {
-		if (GetComponentInParent<Tank1> () == null) {
-			water.fillAmount = (float)(GetComponentInParent<Tank2> ().CountWater / 30f);
-
-		} else {
-			water.fillAmount = (float)(GetComponentInParent<Tank1> ().CountWater / 30f);
-		}
-
+		
 
 	}
 }
