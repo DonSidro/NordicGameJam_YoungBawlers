5df2b9154eefd5ffea86879c71f116ee7bce0c46
Peter Scheel <Peter Scheel>
2016-04-10 04:59:48 +0200

    Milestone

--- Assets/ReadyArea.cs
+++ Assets/ReadyArea.cs
@@ -1,13 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ReadyArea : MonoBehaviour {
 
-	private int[] readyPlayers;
+	private ArrayList readyPlayers = new ArrayList();
+	public Text countdown;
+	private float TimeToStart = 5;
 
 	// Use this for initialization
 	void OnTriggerEnter2D(Collider2D col){
 		int id = col.GetComponent<Player> ().player;
-		print (id);
+		readyPlayers.Add (id);
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		print ("WTF?");
+		countdown.text = "Starting in ... 5";
+		int id = col.GetComponent<Player> ().player;
+		readyPlayers.Remove (id);
+	}
+
+	void Update(){
+		if (readyPlayers.Count >= 2) {
+			TimeToStart -= Time.deltaTime;
+			countdown.enabled = true;
+		} else {
+			TimeToStart = 5;
+			countdown.enabled = false;
+		}
+		if (TimeToStart <= 0) {
+			countdown.text = "Starting ...";
+			SceneManager.LoadScene (1);
+		}
+
+		if(TimeToStart > 0){
+			countdown.text = "Starting in ... " + TimeToStart.ToString ("F0");
+		}
 	}
 }
